f7f77fae545527d334cce80f6d376bed19e7e370
deni1gugu <150838197+deni1gugu@users.noreply.github.com>
2024-10-25 22:58:47 +0300

    2 парс
    ааа

--- cian.py
+++ cian.py
@@ -1,6 +1,6 @@
 import cianparser
 
-moscow_parser = cianparser.CianParser(location="Химки")
-data = moscow_parser.get_flats(deal_type="sale", rooms=2, with_saving_csv=True, additional_settings={"start_page":1, "end_page":20}, with_extra_data = True)
+moscow_parser = cianparser.CianParser(location="Долгопрудный")
+data = moscow_parser.get_flats(deal_type="sale", rooms=1, with_saving_csv=True, additional_settings={"start_page":1, "end_page":20}, with_extra_data = True)
 
 print(data[0])
