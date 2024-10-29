e191cafce5c793363de63d13784453ba47076a0b
mrsoning <artemryzkov02@gmail.com>
2024-10-29 12:16:02 +0300

    last
    last

--- cian.py
+++ cian.py
@@ -4,3 +4,5 @@ moscow_parser = cianparser.CianParser(location="Долгопрудный")
 data = moscow_parser.get_flats(deal_type="sale", rooms=1, with_saving_csv=True, additional_settings={"start_page":1, "end_page":20}, with_extra_data = True)
 
 print(data[0])
+
+ 
